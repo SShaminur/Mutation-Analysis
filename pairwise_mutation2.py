16308aa864ac9f8979fc91beb087f4460de61e57
Shaminur <43230580+SShaminur@users.noreply.github.com>
2020-10-30 14:30:12 +0600

    Add files via upload

---
+++ pairwise_mutation2.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# Count how many gaps and mismatches using pairwise2 alignment v2
+# python pairwise_custom.py your_fasta_file.fasta
+
+import itertools, sys
+from Bio import SeqIO, pairwise2
+from Bio.Align import substitution_matrices
+
+matrix = substitution_matrices.load("BLOSUM62")
+
+def my_format_alignment(w, align1, align2, score, begin, end):
+    z = 1
+    for a, b in zip(align1[begin:end], align2[begin:end]):
+        if a == b:
+            pass
+        elif a == "-" or b == "-":
+            print(' ' + IDs[w + 1] + ' ' + a + str(z) + b)
+        else:
+            print(' ' + IDs[w + 1] + ' ' + a + str(z) + b)
+        z += 1
+
+FastaFile = open(sys.argv[1], 'U')
+
+SEQs = []
+IDs = []
+for rec in SeqIO.parse(FastaFile, 'fasta'):
+    IDs.append (rec.id)
+    SEQs.append(rec.seq)
+
+combos = itertools.combinations(SEQs, 2)
+
+z = 0
+for k, v in combos:
+    if z < (len(SEQs) - 1):
+        aln = pairwise2.align.globalds(k, v, matrix, -10, -1)
+        my_format_alignment(z, *aln[0])
+    z += 1
+
